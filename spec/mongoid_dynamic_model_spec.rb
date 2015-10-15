e80f9d107dfd6049f9921f10bb70be0faa079b19
Timothy Chung <timchunght@gmail.com>
2015-10-15 12:45:19 -0400

    Added collections test

--- spec/mongoid_dynamic_model_spec.rb
+++ spec/mongoid_dynamic_model_spec.rb
@@ -9,4 +9,10 @@ describe Mongoid::DynamicModel do
     expect(custom_model.name).to eq "first_custom_model"
     remove_objects(CustomModel)
   end
+
+  it "lists collections in the current configured database (connection testing)" do
+  	db = Mongoid.default_client.database
+		puts db.collection_names.join(",")
+	end
+	
 end
